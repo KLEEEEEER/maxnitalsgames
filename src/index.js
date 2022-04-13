704d8838f227a61101a274e42942df38e205fdc3
KLEEEEEER <gonzalx_747@mail.ru>
2022-04-13 18:36:09 +0900

    Changed projects heights

--- src/index.js
+++ src/index.js
@@ -30,3 +30,27 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+console.log("window.innerWidth = " + window.innerWidth);
+if (window.innerWidth > 768) {
+    let projectsLists = document.querySelectorAll(".projects-list");
+    console.log("Found " + projectsLists.length + " projects lists");
+    projectsLists.forEach((list) => {
+        let elements = list.querySelectorAll(".gameproject");
+
+        for (let i = 0; i < elements.length; i+=2) {
+            if (typeof elements[i + 1] === 'undefined') continue;
+
+            console.log(elements[i].style);
+            let height1 = elements[i].offsetHeight;
+            let height2 = elements[i + 1].offsetHeight;
+
+            if (height1 > height2) {
+                elements[i + 1].style.height = elements[i].offsetHeight + "px";
+            }
+            else {
+                elements[i].style.height = elements[i + 1].offsetHeight + "px";
+            }
+        }
+    });
+}
